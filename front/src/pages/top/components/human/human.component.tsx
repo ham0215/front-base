12d482d09fe18895261b61c0fc3a3d85c935bcbd
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-04-17 01:29:54 +0900

    Add tailwind

--- front/src/pages/top/components/human/human.component.tsx
+++ front/src/pages/top/components/human/human.component.tsx
@@ -6,8 +6,8 @@ type Props = {
 export function Human({ data, onStartEdit }: Props) {
   return (
     <div>
-      <div>
-        <button onClick={onStartEdit}>Edit</button>
+      <div className="bg-red-50">
+        <button className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base" onClick={onStartEdit}>Edit</button>
       </div>
       <div>
         {data.human.id}
